f40ac2ff80e0f700aa2bf3a3c19643bbebeb1d03
StormerD <drodwell2005@gmail.com>
2025-04-15 00:42:16 +1000

    chapter 6

--- Roguelilke/actions.py
+++ Roguelilke/actions.py
@@ -1,41 +1,95 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Optional, Tuple, TYPE_CHECKING
 
 if TYPE_CHECKING:
   from engine import Engine
-  from entity import Entity
+  from entity import Actor, Entity
 
 class Action:
-  def perform(self, engine: Engine, entity: Entity) -> None:
+  def __init__(self, entity: Actor) -> None:
+    super().__init__()
+    self.entity = entity
+    
+  @property
+  def engine(self) -> Engine:
+    """ return the engine this action belongs to """
+    return self.entity.gamemap.engine
+  
+  def perform(self) -> None:
     """ perform this action with the object needed to determine its scope
     
-    'engine' is the scope this action is being performed in
+    'self.engine' is the scope this action is being performed in
     
-    'entity' is the object performing the action
+    'self.entity' is the object performing the action
     
     this method must be overridden by Action subclasses
     """
     raise NotImplementedError()
 
 class EscapeAction(Action):
-  def perform(self, engine: Engine, entity: Entity) -> None:
+  def perform(self) -> None:
     raise SystemExit()
+  
+class WaitAction(Action):
+  def perform(self) -> None:
+    pass
 
-class MovementAction(Action):
-  def __init__(self, dx: int, dy: int):
-    super().__init__()
-
+class ActionWithDirection(Action):
+  def __init__(self, entity: Actor, dx: int, dy: int):
+    super().__init__(entity)
     self.dx = dx
     self.dy = dy
     
-  def perform(self, engine: Engine, entity: Entity) -> None:
-    dest_x = entity.x + self.dx
-    dest_y = entity.y + self.dy
-
-    if not engine.game_map.in_bounds(dest_x, dest_y):
-      return # Destination is out of bounds.
-    if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
-      return # Destination is blocked by a tile.
+  @property
+  def dest_xy(self) -> Tuple[int, int]:
+    """ returns this action's destination """
+    return self.entity.x + self.dx, self.entity.y + self.dy
+  
+  @property
+  def blocking_entity(self) -> Optional[Entity]:
+    """ return the blocking entity at this action's destination """
+    return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
+  
+  @property
+  def target_actor(self) -> Optional[Actor]:
+    """ return the actor at this action's destination """
+    return self.engine.game_map.get_actor_at_location(*self.dest_xy)
     
-    entity.move(self.dx, self.dy)
+  def perform(self) -> None:
+    raise NotImplementedError()
+
+class MeleeAction(ActionWithDirection):
+  def perform(self) -> None:
+    target = self.target_actor
+    if not target:
+      return # no entity to attack
+    
+    damage = self.entity.fighter.power - target.fighter.defense
+
+    attack_desc = f"{self.entity.name.capitalize()} attacks {target.name}"
+    if damage > 0:
+      print(f"{attack_desc} for {damage} hit points.")
+      target.fighter.hp -= damage
+    else:
+      print(f"{attack_desc}, but does no damage.")
+
+class MovementAction(ActionWithDirection):
+  def perform(self) -> None:
+    dest_x, dest_y = self.dest_xy
+    
+    if not self.engine.game_map.in_bounds(dest_x, dest_y):
+      return # Destination is out of bounds.
+    if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
+      return # Destination is blocked by a tile.
+    if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+      return # destination is blocked by an entity
+    
+    self.entity.move(self.dx, self.dy)
+    
+class BumpAction(ActionWithDirection):
+  def perform(self) -> None:
+    if self.target_actor:
+      return MeleeAction(self.entity, self.dx, self.dy).perform()
+    else:
+      return MovementAction(self.entity, self.dx, self.dy).perform()
--- Roguelilke/main.py
+++ Roguelilke/main.py
@@ -1,9 +1,10 @@
+import copy
+
 import tcod
 
 from engine import Engine
-from entity import Entity
-from game_map import GameMap
-from input_handlers import EventHandler
+import entity_factories
+from procgen import generate_dungeon
 
 def main() -> None:
   screen_width = 80
@@ -12,19 +13,31 @@ def main() -> None:
   map_width = 80
   map_height = 45
   
+  room_min_size = 6
+  room_max_size = 10
+  max_rooms = 30
+  
+  max_monsters_per_room = 2
+  
   tileset = tcod.tileset.load_tilesheet(
     "graphics.png", 32, 8, tcod.tileset.CHARMAP_TCOD
   )
 
-  event_handler = EventHandler()
+  player = copy.deepcopy(entity_factories.player)
   
-  player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-  npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
-  entities = {npc, player}
+  engine = Engine(player=player)
   
-  game_map = GameMap(map_width, map_height)
-  
-  engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+  engine.game_map = generate_dungeon(
+    max_rooms=max_rooms,
+    room_min_size=room_min_size,
+    room_max_size=room_max_size,
+    map_width=map_width,
+    map_height=map_height,
+    max_monsters_per_room=max_monsters_per_room,
+    engine=engine,
+  )
+
+  engine.update_fov()
   
   with tcod.context.new_terminal(
     screen_width,
@@ -37,9 +50,6 @@ def main() -> None:
     while True:
       engine.render(console=root_console, context=context)
       
-      events = tcod.event.wait()
-      
-      engine.handle_events(events)
-  
+      engine.event_handler.handle_events()  
 if __name__ == "__main__":
   main()
