b39b1eb24466f8b1bb9e16de78794047721ad14e
StormerD <drodwell2005@gmail.com>
2025-07-08 10:41:38 +1000

    working on  hangman

---
+++ Dylan_Website/scripts/hangman.js
@@ -0,0 +1,13 @@
+var word;
+var attempts;
+var guessedLetters;
+
+// function sets up the game
+function setGame() {
+
+}
+
+// sets up game on window load
+window.onload = function() {
+  setGame();
+}
